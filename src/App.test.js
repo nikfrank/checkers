77f3767d9bec8fd9c109e99e52966675d89ea3c4
nik frank <nikbfrank@gmail.com>
2019-03-05 16:36:32 +0200

    more tests

--- src/App.test.js
+++ src/App.test.js
@@ -15,15 +15,40 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('mounts to enzyme and selects a piece', ()=>{
+
+it('mounts to enzyme to render a Board', ()=>{
+
+  const fakePieces = [[ 'p1', null ], [null, 'p2']];
+  const fakeMoves = [[ false, false ], [ false, false ] ];
+
+  const p = mount(<Board pieces={fakePieces}
+                         moves={fakeMoves} />);
+
+  expect( p.find('.Board') ).toHaveLength( 1 );
+});
+
+it('mounts to enzyme and clicks a cell', ()=>{
 
   const clickSpy = jest.fn();
+
+  const fakePieces = [[ 'p1', null ], [null, 'p2']];
+  const fakeMoves = [[ false, false ], [ false, false ] ];
+
   
-  const p = mount(<Board pieces={[[ 'p1', null ], [null, 'p2']]}
-                         moves={[[ false, false ], [ false, false ] ]}
+  const p = mount(<Board pieces={fakePieces}
+                         moves={fakeMoves}
                          onClick={clickSpy}/>);
 
   p.find('div.BoardCell').first().simulate('click');
 
-  expect( clickSpy ).toHaveBeenCalled();
+  expect( clickSpy.mock.calls ).toHaveLength( 1 );
+  expect( clickSpy.mock.calls[0][0] ).toEqual( 0 );
+  expect( clickSpy.mock.calls[0][1] ).toEqual( 0 );
+
+  p.find('div.BoardCell').at(2).simulate('click');
+
+  expect( clickSpy.mock.calls ).toHaveLength( 2 );
+  expect( clickSpy.mock.calls[1][0] ).toEqual( 0 );
+  expect( clickSpy.mock.calls[1][1] ).toEqual( 1 );
+
 });
